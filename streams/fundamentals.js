575ff8bd5d3677f52e5e77c3ed693b535ea6e89f
Rafael Manzo Thosi <thhrafael@outlook.com>
2026-04-20 16:32:19 -0300

    add: streams/fundamentals.js

---
+++ streams/fundamentals.js
@@ -0,0 +1,13 @@
+// Netflix & Spotify
+
+// Importação de clientes via CSV (Excel)
+// 1gb - 1.000.000
+// POST /upload import.csv
+
+// 10mb/s - 100s
+
+// 100s -> Inserções no banco de dados
+
+// 10mb/s -> 10.0000
+
+// Readble Streams / Writable Streams
