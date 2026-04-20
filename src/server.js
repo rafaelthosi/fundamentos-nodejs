bd1c6db79110576a3bb3a5eb917722ddc6631ac2
Rafael Manzo Thosi <thhrafael@outlook.com>
2026-04-20 15:46:47 -0300

    update: salvando users em memoria e retornando header content-type no GET

--- src/server.js
+++ src/server.js
@@ -12,14 +12,26 @@ import http from 'node:http'
 // PATCH => Atualizar uma informação específica de um recurso no back-end
 // DELETE => Deletar um recurso do back-end
 
+// Stateful - Stateless
+
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
   
   if (method === 'GET' && url === '/users') {
-    return res.end('Hello World')
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Rafael Manzo Thosi',
+      email: 'teste@teste.com'
+    })
+
     return res.end('Hello World')
   }
 })
