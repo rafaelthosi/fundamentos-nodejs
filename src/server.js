68e0d223880ca85fb328c661bd97b0681577a17c
Rafael Manzo Thosi <thhrafael@outlook.com>
2026-04-20 15:38:39 -0300

    update: comentarios sobre method e url no server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,27 @@
 import http from 'node:http'
 
-const server = http.createServer((request, response) => {
-  return response.end('Hello World')
+// - HTTP
+//  - Método HTTP
+//  - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req
+  
+  if (method === 'GET' && url === '/users') {
+    return res.end('Hello World')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    return res.end('Hello World')
+  }
 })
 
 server.listen(3333)
